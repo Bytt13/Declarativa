b74f646c1c43a2d3e7222e81cef8c1a56ff572f8
Lucas Chaves <95041440+Bytt13@users.noreply.github.com>
2025-02-06 19:08:06 -0300

    Add files via upload

--- main.scala
+++ main.scala
@@ -1,25 +1,8 @@
-object SistemaBiblioteca {
-  case class Livro(titulo: String, autor: String, disponivel: Boolean)
-
-  def buscarPorTitulo(livros: List[Livro], titulo: String): Option[Livro] =
-    livros.find(_.titulo.equalsIgnoreCase(titulo))
-
-  def emprestarLivro(livros: List[Livro], titulo: String): List[Livro] =
-    livros.map {
-      case livro if livro.titulo.equalsIgnoreCase(titulo) && livro.disponivel => livro.copy(disponivel = false)
-      case livro => livro
-    }
-
-  def listarLivros(livros: List[Livro]): String =
-    livros.map(l => s"${l.titulo} - Autor: ${l.autor}, Disponível: ${l.disponivel}").mkString("\n")
+object ConverterEmail {
+  def converterParaEmail(nome: String, dominio: String): String =
+    nome.trim.toLowerCase.replaceAll(" ", ".") + s"@$dominio"
 
   def main(args: Array[String]): Unit = {
-    val biblioteca = List(
-      Livro("Scala Programming", "Martin Odersky", true),
-      Livro("Functional Programming", "Paul Chiusano", true)
-    )
-    val atualizado = emprestarLivro(biblioteca, "Scala Programming")
-    println(listarLivros(atualizado))
+    println(converterParaEmail("João da Silva", "gmail.com"))
   }
 }
-
